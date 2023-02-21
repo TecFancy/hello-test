32400deff011c2712bcb99eedb47f276efcb4c81
smpower <rf.wangchn@foxmail.com>
2023-02-21 15:25:40 +0800

    feat: 配置 vscode 短点调试环境

--- packages/jsdom-samples/__tests__/index.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const jsdomSamples = require('../lib');
-const assert = require('assert').strict;
-
-assert.strictEqual(jsdomSamples(), 'Hello from jsdomSamples');
-console.info("jsdomSamples tests passed");
--- packages/node-samples/README.md
+++ packages/node-samples/README.md
@@ -1,10 +1,10 @@
-# `node-samples`
+# `Jest - node-samples`
 
 > TODO: description
 
 ## Usage
 
-```
+```none
 const nodeSamples = require('node-samples');
 
 // TODO: DEMONSTRATE API
