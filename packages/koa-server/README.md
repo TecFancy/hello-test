929be740c4ace7270ac9d6e1345360a6515d161b
smpower <rf.wangchn@foxmail.com>
2023-02-22 11:49:02 +0800

    feat: koa server

---
+++ packages/koa-server/README.md
@@ -0,0 +1,11 @@
+# `@hello-test/koa-server`
+
+> TODO: description
+
+## Usage
+
+```
+const koaServer = require('@hello-test/koa-server');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/koa-server/src/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const Koa = require("koa");
+const Router = require("@koa/router");
+const { koaBody } = require("koa-body");
+
+const app = new Koa();
+const router = new Router();
+
+router.post("/users", (ctx) => {
+  const { name } = ctx.request?.body;
+  if (!name) {
+    ctx.body = { status: 'error', msg: 'Please provide a `name` parameter.' }
+  } else {
+    ctx.body = { name, email: `${name.toLowerCase()}@foxmail.com` };
+  }
+});
+
+app.use(koaBody());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(10086, () => {
+  console.log("Server is running on port 10086");
+});
